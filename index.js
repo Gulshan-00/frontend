3d6e00856ca66b4604412b5ee8170f748849750a
Gulshan-00 <90376979+Gulshan-00@users.noreply.github.com>
2024-05-28 09:28:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+
+//This is closers Video number 9;
+                // var a = 5;
+                // setname();
+
+                // function getname() {
+                // var x = 10;
+                // console.log(x);
+                //     function innerfun() {
+                //         var z = 89;
+                //         console.log(z);
+                //         // console.log(x);
+                //             function innerfun2() {
+                //             var Q = 999;
+                //             console.log(Q);
+                //             console.log(z);
+                //             console.log(x);
+                //             }
+                //             innerfun2();
+                //         }
+                // innerfun();
+                // }
+                // getname();
+
+                // function setname() {
+                // var y = 20;
+                // console.log(y);
+                // }
+
+                // console.log(a);
+
+//This is setTimeout with the closers
+//setTimeout + closers
+
+
+              // function x()
+              // {
+
+              //   for(var i=0;i<=5;i++)
+              //     {
+
+              //       function close(x)
+              //       {
+              //         setTimeout( function () { console.log(x);},x*1000);
+              //       }
+
+              //       close(i);
+                    
+              //     }
+              
+              //     console.log("Hey Gulshan Your are trying to learn the javascript");
+                  
+                 
+
+              // }
+              // x();
+
+
+// const users = [
+//   { firstName: "akshay", lastName: "saini", age: 26 },
+//   { firstName: "donald", lastName: "trump", age: 75 },
+//   { firstName: "elon", lastName: "musk", age: 50 },
+//   { firstName: "deepika", lastName: "padukone", age: 26 },
+//   ];
+
+
+//   const output=users.reduce(function(acc,curr){
+
+//     if(curr.age<30){
+//       acc.push(curr.firstName);
+//     }
+    
+//     return acc;
+
+//   },[])
+
+//   console.log(output);
+
+
+
+// const name="https://www.w3schools.com/html/html_tables.asp";
+// const user=fetch(name);
+// console.log(user);               
+
+
+const id=Symbol('123');
+const newif=Symbol('123');
+
+console.log(typeof id);
+console.log(typeof newif);
+
+console.log(id===newif);
+                
+              
+
+
